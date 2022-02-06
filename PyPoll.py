a5623de3d416c8b2d5994b8a0de10b38d1317aba
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-02-06 14:33:53 -0500

    3.5.2

--- PyPoll.py
+++ PyPoll.py
@@ -15,13 +15,29 @@ file_to_load = os.path.join("Resources","election_results.csv")
 #Assign a variable for the file to save.
 file_to_save = os.path.join("analysis", "elections_analysis.txt")
 
+# Inititize a total vote counter.
+total_votes = 0
+
+
+candidates_options=[]
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-# To do: read and analyze data.
     #read the file object 
     file_reader= csv.reader(election_data)
 
-    # Print Header row. 
+# To do: read and analyze data.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print Header row. 
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes+=1
+        #Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # Add the dandidates name to the dandidate list
+        candidates_options.append(candidate_name)
+
+print(candidates_options)
