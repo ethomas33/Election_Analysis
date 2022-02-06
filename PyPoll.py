11edb2ebc30a192537b2776d02e15eb50003719a
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-02-06 13:03:13 -0500

    updates 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -6,26 +6,22 @@
 #The winner of the election based on popular vote
 #Resources/election_results.csv
 
+# Add dependencies.
 import csv
 import os
 
-# Assign a variable for the file to load and the path.
+# Assign a variable for the file to load.
 file_to_load = os.path.join("Resources","election_results.csv")
+#Assign a variable for the file to save.
+file_to_save = os.path.join("analysis", "elections_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-# To do: perform analysis.
-    print(election_data)
+# To do: read and analyze data.
+    #read the file object 
+    file_reader= csv.reader(election_data)
 
-#Creat a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "elections_analysis.txt")
-
-#Using the open() function with the "w" mode we will write data to the file.
-outfile = open(file_to_save,"w")
-
-#write some data to the file
-outfile.write("Hello World")
-
-#close the file
-outfile.close()
+    # Print Header row. 
+    headers = next(file_reader)
+    print(headers)
